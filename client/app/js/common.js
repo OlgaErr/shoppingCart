bc77e6f1ecc00a2d2cd91000eebd2bace1fd0ad9
Olya <eremenko_olya@mail.ru>
2019-05-10 20:55:39 +0300

    form for creating  lists added

---
+++ client/app/js/common.js
@@ -0,0 +1,9 @@
+function openForm() {
+  const form = document.getElementById('form');
+  form.style.display = 'block';
+}
+
+function closeForm() {
+  const form = document.getElementById('form');
+  form.style.display = 'none';
+}
