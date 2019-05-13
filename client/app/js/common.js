b1b5c61ed3c3891bb913cc7db6f12bad3a7e4e4c
Olya <eremenko_olya@mail.ru>
2019-05-13 14:16:52 +0300

    before pull

--- client/app/js/common.js
+++ client/app/js/common.js
@@ -2,21 +2,33 @@ function setFocus(elem) {
   elem.focus();
 }
 
+function hideElem(elem) {
+  elem.style.display = 'none'; 
+}
+
+function showElem(elem) {
+  elem.style.display = 'block'; 
+}
+
 function openForm() {
   const form = document.getElementById('form');
   form.style.display = 'block';
   const focusOn = document.getElementById('formGroupTitle');
-  
+  const btnPlus = document.getElementsByClassName('myBtn')[0];
+
+  hideElem(btnPlus);
   setFocus(focusOn);
 }
 
 function closeForm() {
   const form = document.getElementById('form');
   form.style.display = 'none';
+  const btnPlus = document.getElementsByClassName('myBtn')[0];
+
+  showElem(btnPlus);
 }
 
 function create() {
   const spinner = document.getElementById('form__spinner');
   spinner.style.display = 'block';
 }
-
